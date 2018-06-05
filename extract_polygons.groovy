7d7d00d02b0de983ecfbe723b17def0d5cfb67f2
ethan <ethan.weinberger@yale.edu>
2018-06-05 11:18:11 -0400

    Added capability to extract multiple annotations from one file

--- extract_polygons.groovy
+++ extract_polygons.groovy
@@ -1,20 +1,25 @@
-def path = buildFilePath(PROJECT_BASE_DIR, 'annotation_csv_files')
-mkdirs(path)
+def top_level_path = buildFilePath(PROJECT_BASE_DIR, 'annotation_csv_files')
+mkdirs(top_level_path)
 
-def name = getProjectEntry().getImageName() + '.csv'
-path = buildFilePath(path, name)
-def file = new File(path)
-file.text = ''
+def image_name = getProjectEntry().getImageName()
+image_path = buildFilePath(top_level_path, image_name)
+mkdirs(image_path)
 
 def annotation_objects = getAnnotationObjects()
-if (annotation_objects.size() > 0) {
-    // Assuming that our first annotation object is the only one we care about
-    def annotation = getAnnotationObjects()[0]
-    def roi = annotation.getROI()
+def counter = 0
+
+for (annotation_object in annotation_objects) {
     
-    if (annotation != null) {
-        for (point in roi.getPolygonPoints()) {
-            file << point.getX() << ',' << point.getY() << System.lineSeparator()
-        }
+    name = counter.toString() + '.csv'
+    annotation_path = buildFilePath(image_path, name)
+    def file = new File(annotation_path)
+    file.text = ''
+
+    // Assuming that our first annotation object is the only one we care about
+    def roi = annotation_object.getROI()
+    
+    for (point in roi.getPolygonPoints()) {
+        file << point.getX() << ',' << point.getY() << System.lineSeparator()
     }
+    counter++
 }
