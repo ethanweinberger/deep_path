ceda907099e8fe1eb69b6e31628b78177a76e371
ethan <ethan.weinberger@yale.edu>
2018-08-05 22:03:18 -0400

    Modified polygon script for bone marrow annotation scheme

--- extract_polygons.groovy
+++ extract_polygons.groovy
@@ -1,18 +1,41 @@
-def top_level_path = buildFilePath(PROJECT_BASE_DIR, 'annotation_csv_files')
+def top_level_path        = buildFilePath(PROJECT_BASE_DIR, 'annotation_csv_files')
+def stroma_path           = buildFilePath(top_level_path, 'stroma_csv_files')
+def large_cell_tumor_path = buildFilePath(top_level_path, 'large_cell_tumor_csv_files')
+def small_cell_tumor_path = buildFilePath(top_level_path, 'small_cell_tumor_csv_files')
+
 mkdirs(top_level_path)
+mkdirs(stroma_path)
+mkdirs(large_cell_tumor_path)
+mkdirs(small_cell_tumor_path)
 
 def image_name = getProjectEntry().getImageName()
-image_path = buildFilePath(top_level_path, image_name)
-mkdirs(image_path)
+stroma_image_path = buildFilePath(stroma_path, image_name)
+large_cell_tumor_image_path = buildFilePath(large_cell_tumor_path, image_name)
+small_cell_tumor_image_path = buildFilePath(small_cell_tumor_path, image_name)
+
+mkdirs(stroma_image_path)
+mkdirs(large_cell_tumor_image_path)
+mkdirs(small_cell_tumor_image_path)
 
 def annotation_objects = getAnnotationObjects()
 def counter = 0
 
 for (annotation_object in annotation_objects) {
-    
+      
+    //metaClass.methods*.name.sort().unique()
     name = counter.toString() + '.csv'
-    annotation_path = buildFilePath(image_path, name)
-    def file = new File(annotation_path)
+    if (annotation_object.getPathClass() == getPathClass("Tumor")) {
+        annotation_dir = large_cell_tumor_image_path
+    }
+    else if (annotation_object.getPathClass() == getPathClass("Stroma")) {
+        annotation_dir = stroma_image_path
+    }
+    else {
+        annotation_dir = small_cell_tumor_image_path
+    }
+    
+    annotation_file = buildFilePath(annotation_dir, name)
+    def file = new File(annotation_file)
     file.text = ''
 
     // Assuming that our first annotation object is the only one we care about
